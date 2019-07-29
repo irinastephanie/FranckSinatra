a2fae949561e0b72f09aad477f49306c9b40b3f6
irinastephanie <stephanieiirn@gmail.com>
2019-07-29 17:39:54 +0200

    premiere version

---
+++ lib/controller.rb
@@ -0,0 +1,25 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  #class ApplicationController qui hérite de la classe sinatra:base
+  get '/' do
+    #pour exeecuter le code qui suit
+    #"<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+    erb :index, locals: {gossips: Gossip.all}
+  end 
+  #run! if app_file == $0
+  #execute le code de la classe sans avoir à ecrire ApplicationController.new.perform
+  #c'est l'execution de la méthode pour lancer la machine
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+  post '/gossips/new/' do 
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+  #puts "Salut, je suis dans le serveur"
+  #puts "Ceci est le contenue du hash params : #{params}"
+  #puts "Trop bien! et cei est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+  #puts "De la bombe, ça c'est le contenue : #{params["gossip_content"]}"
+  #puts "ca déchire"
+  redirect '/'
+  end
+end 
